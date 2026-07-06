e736160057b3b50353ee1c27836bfbe752a4546a
Viplove Rajora <rajoraviplove21@gmail.com>
2026-07-07 00:10:00 +0530

    undo

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,9 +1,7 @@
 import axios from 'axios'
 
-
-
 const api = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5001/api',
   withCredentials: true,
   headers: { 'Content-Type': 'application/json' },
 })
