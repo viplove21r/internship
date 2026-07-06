99c4bd6cafa45f7b76f7577b2081b42c643e767c
Viplove Rajora <rajoraviplove21@gmail.com>
2026-07-06 23:59:40 +0530

    changed axos.js

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
+
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5001/api',
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
   withCredentials: true,
   headers: { 'Content-Type': 'application/json' },
 })
